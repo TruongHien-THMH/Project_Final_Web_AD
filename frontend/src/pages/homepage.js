84c92dfdb2ea291d06c91b78f9a788a9edec1ab9
Hoang271205 <hoang.pham230217@vnuk.edu.vn>
2025-11-10 21:00:19 +0700

    add anh va sua cau truc homepage va app

---
+++ frontend/src/pages/homepage.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+import NowDisplay from '../components/NowDisplay';
+
+function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <NowDisplay />
+    </>
+  );
+}
+
+export default HomePage;
