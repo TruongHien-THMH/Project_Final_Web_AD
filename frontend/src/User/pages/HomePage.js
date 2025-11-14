163ca8cd85ee0d07c9a8c736a1eafac2bffd4262
Nguyen Tri <dontringuyen10@gmail.com>
2025-11-14 14:31:07 +0700

    Dmm

--- frontend/src/User/pages/HomePage.js
+++ frontend/src/User/pages/HomePage.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD:frontend/src/User/pages/HomePage.js
 
 
 import React from 'react';
-=======
->>>>>>> 671fb7ce4b4265016ac4229da41d1f9c02928e73:frontend/src/pages/homepage.js
 import HeroSection from '../components/HeroSection';
 import NowDisplay from '../components/NowDisplay';
 import Display from '../components/Display';
