4b157fbf2c105fea22b7015b70aa6fde8e1a5b91
HienTruong <hien0934752432@gmail.com>
2025-11-15 13:13:49 +0700

    Refactor backend routes and database connection; add frontend movie display components

--- backend/controllers/MovieControllers.js
+++ backend/controllers/MovieControllers.js
@@ -1,10 +1,11 @@
-const Moive = require("../models/Movie");
+const Movie = require("../models/Movie");
 
 exports.getNowPlayingMovie = async (req, res) => {
     try{
         // Lấy dữ liệu từ database
-        const movies = await Moive.find();
-        res.json(movies);
+        // const movies = await Moive.find();
+        // res.json(movies);
+        res.json({messgae: 'Đã gọi được controller'})
     } catch (err) {
         console.log("Lỗi khi cố lấy dữ liệu của API phim đang chiếu:");
         console.error(err);
