d2fc594755f1fccdeec168cf242cbd2dc6ce628f
HienTruong <hien0934752432@gmail.com>
2025-11-15 13:58:23 +0700

    Remove unused movieRouter import and update HomePage to include BEDisplay component

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const movieRouter = require('./routes/movieRoutes');
 const showRoutes = require("./routes/showRoutes");
 
 dotenv.config();
--- frontend/src/User/pages/HomePage.js
+++ frontend/src/User/pages/HomePage.js
@@ -4,12 +4,15 @@ import React from 'react';
 import HeroSection from '../components/HeroSection';
 import NowDisplay from '../components/NowDisplay';
 import Display from '../components/Display';
+import BEDisplay from '../components/BEDisplay';
 function HomePage() {
   return (
     <>
       <HeroSection />
-      {/* <NowDisplay /> */}
-      <Display/>
+      {/* <NowDisplay /> */} 
+      <Display/> 
+      <span className='text-xl font-bold'>Demo BE API</span>
+      <BEDisplay/>
     </>
   );
 }
