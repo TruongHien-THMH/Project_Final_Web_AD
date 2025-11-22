4b5e7f9c03cb26101f48d67c33e92fa9c2a130ac
HienTruong <hien0934752432@gmail.com>
2025-11-22 14:27:19 +0700

    Update lại movie_card // Tái cấu trúc thư mục FE (UI-Các Component tái sử dụng)

--- frontend/src/modules/Admin/pages/AdminDashboardPage.js
+++ frontend/src/modules/Admin/pages/AdminDashboardPage.js
@@ -1,18 +1,11 @@
 // src/admin/pages/AdminDashboardPage.js
 import React, {useEffect, useState }from 'react';
 import StatsCard from '../components/StatsCard';
-// import MovieCard from '../../../ components/ui/MovieCard_user';
-import MovieCard from '../../Admin/components/MovieCard';
+import MovieCard from '../../../ components/ui/MovieCard_user';
+// import MovieCard from '../../Admin/components/MovieCard';
 import API from '../../../api/ApiClient';
 
 const AdminDashboardPage = () => {
-  const statsData = [
-    { title: 'Total Bookings', value: 73, icon: 'receipt' },
-    { title: 'Total Revenue', value: '$1,060', icon: 'bag' },
-    { title: 'Active Movies', value: 3, icon: 'movie' },
-    { title: 'Total Users', value: 43, icon: 'user' },
-  ];
-
   // Dữ liệu giả định cho Active Movies
   const activeMovies = [
     { title: 'Alita Battle Angel 4k 2019 Movies', rating: 4.5, price: 29 },
@@ -24,9 +17,30 @@ const AdminDashboardPage = () => {
 
   useEffect(() => {
     const fetchMovieData = async () => {
-      const data = await API.get()
+      try {
+        const res = await API.get('/');
+        console.log("Dữ liệu lấy từ API: ", res);
+
+        const movie = res.data;
+        console.log("Convert Dữ liệu: ", movie);
+
+        setDataBuffer(movie);
+
+      } catch (error) {
+        console.log("Lỗi từ hàm lấy dữ liệu phim từ Admin_DashBoard: ", error);
+      }
     }
-  })
+
+    fetchMovieData();
+  }, [])
+
+  const statsData = [
+    { title: 'Total Bookings', value: 73, icon: 'receipt' },
+    { title: 'Total Revenue', value: '$1,060', icon: 'bag' },
+    { title: 'Active Movies', value: dataBuffer.length, icon: 'movie' },
+    { title: 'Total Users', value: 43, icon: 'user' },
+  ];
+
 
 
   return (
@@ -43,9 +57,11 @@ const AdminDashboardPage = () => {
       {/* 2. Phim đang hoạt động */}
       <h2 className="text-2xl font-semibold mb-4 text-gray-300">Active Movies</h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-        {activeMovies.map((movie, index) => (
+        {/* {activeMovies.map((movie, index) => (
           <MovieCard key={index} movie={movie} />
-        ))}
+        ))} */}
+
+        <MovieCard data={dataBuffer} />
       </div>
     </div>
   );
